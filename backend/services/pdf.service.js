459077e58559f656ebffc770864176cc46ebf92d
Ritikjaykar <ritikjaykar2@gmail.com>
2026-01-13 00:56:19 +0530

    Fix pdf upload

--- backend/services/pdf.service.js
+++ backend/services/pdf.service.js
@@ -1,10 +1,12 @@
+// pdf.service.js
 import fs from "fs";
-import pdfParse from "pdf-parse";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const pdfParse = require("pdf-parse");
 
 export async function extractTextFromPDF(filePath) {
   const buffer = fs.readFileSync(filePath);
   const data = await pdfParse(buffer);
   return data.text;
 }
-
-
